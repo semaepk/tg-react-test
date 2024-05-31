461161625d02e2ea55b51504ddf3fa4d3bf64b4c
semaep.k <semaep.k@ayagroup.pro>
2024-05-31 21:03:21 +0300

    fast fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-// import Header from "./components/Header/Header";
+import Header from "./components/Header/Header";
 // import {Route, Routes} from 'react-router-dom'
 // import ProductList from "./components/ProductList/ProductList";
 // import Form from "./components/Form/Form";
@@ -15,8 +15,8 @@ function App() {
 
     return (
         <div className="App">
-            {/* <Header />
-            <Routes>
+            <Header />
+            {/* <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
             </Routes> */}
