a35feb719f19d2810220ae07a495ddeba7aec748
semaep.k <semaep.k@ayagroup.pro>
2024-05-31 21:14:26 +0300

    fast fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {user}
+                {user.username}
             </span>
         </div>
     );
