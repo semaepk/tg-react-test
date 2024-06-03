aa003a461b67d64fcd371589606504a8f5387e90
semaep.k <semaep.k@ayagroup.pro>
2024-06-03 10:00:30 +0300

    add fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,16 +24,16 @@ const Header = () => {
       <Button onClick={onClose}>Закрыть</Button>
       <span className={'username'}>
         {['id=', id,
-          'queryId=',queryId,
-          'first_name=',first_name,
-          'last_name=',last_name,
-          'username=',username,
-          'language_code=',language_code,
-          'allows_write_to_pm=',allows_write_to_pm,
-          'photo_url=',photo_url,
-          'auth_date=',auth_date,
-          'is_bot=',is_bot,
-          'hash=',hash]}
+          ' queryId=',queryId,
+          ' first_name=',first_name,
+          ' last_name=',last_name,
+          ' username=',username,
+          ' language_code=',language_code,
+          ' allows_write_to_pm=',allows_write_to_pm,
+          ' photo_url=',photo_url,
+          ' auth_date=',auth_date,
+          ' is_bot=',is_bot,
+          ' hash=',hash]}
       </span>
     </div>
   );
