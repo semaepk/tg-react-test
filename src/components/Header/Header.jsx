dce35fd0a3cf030d99480257c02c3e656174598d
semaep.k <semaep.k@ayagroup.pro>
2024-06-11 18:01:41 +0300

    added ref_id fields

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,40 +2,29 @@ import React from 'react';
 import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 import './Header.css';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Header = () => {
   const {
     id,
-    user,
-    queryId,
-    first_name,
-    last_name,
     username,
-    language_code,
-    allows_write_to_pm,
-    photo_url,
-    auth_date,
-    is_bot,
-    hash,
-    initData,
     onClose } = useTelegram();
+
+  let query = useQuery();
+  let refId = query.get('ref_id');
 
   return (
     <div className={'header'}>
       <Button onClick={onClose}>Закрыть</Button>
       <span className={'username'}>
         {['id=', id,
-          ' queryId=',queryId,
-          ' first_name=',first_name,
-          ' last_name=',last_name,
-          ' username=',username,
-          ' language_code=',language_code,
-          ' allows_write_to_pm=',allows_write_to_pm,
-          ' photo_url=',photo_url,
-          ' auth_date=',auth_date,
-          ' is_bot=',is_bot,
-          '/ hash=', hash,
-          ' initData=', initData]}
+          ' username=', username,
+          ' ref_id=', refId]}
       </span>
     </div>
   );
