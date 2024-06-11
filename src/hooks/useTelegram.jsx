4b12f9d68e059e485d4ed1b2edcb6faafba4b3f4
semaep.k <semaep.k@ayagroup.pro>
2024-06-11 17:55:41 +0300

    remove fields

--- src/hooks/useTelegram.jsx
+++ src/hooks/useTelegram.jsx
@@ -19,17 +19,6 @@ export function useTelegram() {
         onToggleButton,
         tg,
         id: tg.initDataUnsafe?.user?.id,
-        user: tg.initDataUnsafe?.user,
-        queryId: tg.initDataUnsafe?.query_id,
-        first_name: tg.initDataUnsafe?.user?.first_name,
-        last_name: tg.initDataUnsafe?.user?.last_name,
-        username: tg.initDataUnsafe?.user?.username,
-        language_code: tg.initDataUnsafe?.user?.language_code,
-        allows_write_to_pm: tg.initDataUnsafe?.user?.allows_write_to_pm,
-        photo_url:  tg.initDataUnsafe?.user?.photo_url,
-        auth_date: tg.initDataUnsafe?.auth_date,
-        is_bot: tg.initDataUnsafe?.user?.is_bot,
-        hash: tg.initDataUnsafe?.hash,
-        initData: tg.initData
+        username: tg.initDataUnsafe?.user?.username
     }
 }
